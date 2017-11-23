d4b60c5b8da331df3bba0aec48d9a1e28878bcf8
孟凡 <DYA1227@163.com>
2017-11-23 13:44:15 +0800

    Initial Commit

---
+++ MYLabelSpaceAndWordSpace/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MYLabelSpaceAndWordSpace
+//
+//  Created by mengdy on 17/11/23.
+//  Copyright © 2017年 mengdy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MYLabelSpaceAndWordSpaceTests/MYLabelSpaceAndWordSpaceTests.m
@@ -0,0 +1,39 @@
+//
+//  MYLabelSpaceAndWordSpaceTests.m
+//  MYLabelSpaceAndWordSpaceTests
+//
+//  Created by mengdy on 17/11/23.
+//  Copyright © 2017年 mengdy. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MYLabelSpaceAndWordSpaceTests : XCTestCase
+
+@end
+
+@implementation MYLabelSpaceAndWordSpaceTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
